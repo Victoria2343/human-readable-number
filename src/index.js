170b3c22031e832b29dc5ca8a8c3d696f828a70a
Victoria2343 <vikusya234@mail.ru>
2020-10-23 23:59:42 +0300

    init; initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
+    const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+    const dozens = ['zero', 'ten', 'twenty','thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const hundreds = ['hundred'];
+
+    if (number.toString().length === '1') {
+        return ones[0];
+    } else if (number.toString().length === '2' && number.toString()[0] === '1') {
+
+    }
   
 }
