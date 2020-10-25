88537ac06502fd26f69f584ec5949a6250487e1e
Victoria2343 <vikusya234@mail.ru>
2020-10-25 23:05:34 +0300

    fix: change fnc

--- src/index.js
+++ src/index.js
@@ -1,12 +1,33 @@
 module.exports = function toReadable (number) {
-    const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-    const dozens = ['zero', 'ten', 'twenty','thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-    const hundreds = ['hundred'];
+    const ones = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '', 'twenty','thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+   
 
-    if (number.toString().length === '1') {
-        return ones[0];
-    } else if (number.toString().length === '2' && number.toString()[0] === '1') {
+    let numStr = number.toString();
 
+    if (number === 0) {
+        return 'zero';
     }
-  
+
+    if (number < 20) {
+        return ones[number];
+    }
+
+    if (numStr.length === 2) {
+        if (numStr.slice(-1) === '0') {
+            return tens[numStr[0]];
+        } else {
+        return tens[numStr[0]] + ' ' + ones[numStr[1]];
+        }
+    } 
+
+    if (numStr.length === 3) {
+        if (numStr[1] === '0' && numStr[2] === '0') {
+            return ones[numStr[0]] + ' hundred';
+        } else {
+            return ones[numStr[0]] + ' hundred ' + toReadable(+(numStr[1] + numStr[2]));
+        }
+       
+    } 
+   
 }
